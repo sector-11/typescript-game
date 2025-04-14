6d77742277a40900be996296fccbdebc55b6dc6c
jmc <120180071+sector-11@users.noreply.github.com>
2025-04-14 20:09:22 +0100

    feat: add ability to draw rooms

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,68 @@
 import "./style.scss";
+
+const ROOM_WIDTH = 15;
+const ROOM_HEIGHT = 9;
+const TILE_SIZE = 100;
+
+type Room = {
+    terrain: number[][];
+    startEntities: number[][];
+};
+
+const roomMaps: Room = {
+    terrain: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    startEntities: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+};
+
+const canvas = <HTMLCanvasElement>document.getElementById("canvas");
+const context = <CanvasRenderingContext2D>canvas.getContext("2d");
+canvas.width = TILE_SIZE * ROOM_WIDTH;
+canvas.height = TILE_SIZE * ROOM_HEIGHT;
+
+const update = () => {
+    drawTerrain(roomMaps);
+    window.requestAnimationFrame(update);
+};
+
+window.onload = () => {
+    window.requestAnimationFrame(update);
+};
+
+const drawTerrain = (room: Room) => {
+    for (let row = 0; row < ROOM_HEIGHT; row++) {
+        for (let column = 0; column < ROOM_WIDTH; column++) {
+            switch (room.terrain[row][column]) {
+                case 0:
+                    context.fillStyle = "black";
+                    break;
+                case 1:
+                    context.fillStyle = "lightgray";
+                    break;
+                default:
+                    context.fillStyle = "red";
+                    break;
+            }
+            context.fillRect(TILE_SIZE * column, TILE_SIZE * row, TILE_SIZE, TILE_SIZE);
+        }
+    }
+};
