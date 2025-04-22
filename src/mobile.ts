9e5aa385dbc71d7b82e5adcf9047da00b1a51c6e
jmc <120180071+sector-11@users.noreply.github.com>
2025-04-22 09:33:12 +0100

    refactor: remove unused variables

--- src/mobile.ts
+++ src/mobile.ts
@@ -2,66 +2,66 @@ import { shared } from "./shared";
 
 export const initializeButtons = () => {
     const leftPadUp = <HTMLButtonElement>document.getElementById("left-pad-up");
-    leftPadUp.addEventListener("touchstart", (ev) => {
+    leftPadUp.addEventListener("touchstart", () => {
         shared.keys["w"] = true;
     });
-    leftPadUp.addEventListener("touchend", (ev) => {
+    leftPadUp.addEventListener("touchend", () => {
         shared.keys["w"] = false;
     });
 
     const leftPadDown = <HTMLButtonElement>document.getElementById("left-pad-down");
-    leftPadDown.addEventListener("touchstart", (ev) => {
+    leftPadDown.addEventListener("touchstart", () => {
         shared.keys["s"] = true;
     });
-    leftPadDown.addEventListener("touchend", (ev) => {
+    leftPadDown.addEventListener("touchend", () => {
         shared.keys["s"] = false;
     });
 
     const leftPadLeft = <HTMLButtonElement>document.getElementById("left-pad-left");
-    leftPadLeft.addEventListener("touchstart", (ev) => {
+    leftPadLeft.addEventListener("touchstart", () => {
         shared.keys["a"] = true;
     });
-    leftPadLeft.addEventListener("touchend", (ev) => {
+    leftPadLeft.addEventListener("touchend", () => {
         shared.keys["a"] = false;
     });
 
     const leftPadRight = <HTMLButtonElement>document.getElementById("left-pad-right");
-    leftPadRight.addEventListener("touchstart", (ev) => {
+    leftPadRight.addEventListener("touchstart", () => {
         shared.keys["d"] = true;
     });
-    leftPadRight.addEventListener("touchend", (ev) => {
+    leftPadRight.addEventListener("touchend", () => {
         shared.keys["d"] = false;
     });
 
     const rightPadUp = <HTMLButtonElement>document.getElementById("right-pad-up");
-    rightPadUp.addEventListener("touchstart", (ev) => {
+    rightPadUp.addEventListener("touchstart", () => {
         shared.keys["ArrowUp"] = true;
     });
-    rightPadUp.addEventListener("touchend", (ev) => {
+    rightPadUp.addEventListener("touchend", () => {
         shared.keys["ArrowUp"] = false;
     });
 
     const rightPadDown = <HTMLButtonElement>document.getElementById("right-pad-down");
-    rightPadDown.addEventListener("touchstart", (ev) => {
+    rightPadDown.addEventListener("touchstart", () => {
         shared.keys["ArrowDown"] = true;
     });
-    rightPadDown.addEventListener("touchend", (ev) => {
+    rightPadDown.addEventListener("touchend", () => {
         shared.keys["ArrowDown"] = false;
     });
 
     const rightPadLeft = <HTMLButtonElement>document.getElementById("right-pad-left");
-    rightPadLeft.addEventListener("touchstart", (ev) => {
+    rightPadLeft.addEventListener("touchstart", () => {
         shared.keys["ArrowLeft"] = true;
     });
-    rightPadLeft.addEventListener("touchend", (ev) => {
+    rightPadLeft.addEventListener("touchend", () => {
         shared.keys["ArrowLeft"] = false;
     });
 
     const rightPadRight = <HTMLButtonElement>document.getElementById("right-pad-right");
-    rightPadRight.addEventListener("touchstart", (ev) => {
+    rightPadRight.addEventListener("touchstart", () => {
         shared.keys["ArrowRight"] = true;
     });
-    rightPadRight.addEventListener("touchend", (ev) => {
+    rightPadRight.addEventListener("touchend", () => {
         shared.keys["ArrowRight"] = false;
     });
 };
